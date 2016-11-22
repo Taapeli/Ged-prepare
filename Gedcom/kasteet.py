16f8952ce06075a274c6d4a8464da900be1c1474
kkujansuu <kkujansuu@kkujansuu1>
2016-11-22 12:25:07 +0200

    Moved comment to correct file

--- Gedcom/kasteet.py
+++ Gedcom/kasteet.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+"""
+Lähettäjä: <pekka.valta@kolumbus.fi>
+Päiväys: 12. marraskuuta 2016 klo 17.26
+Aihe: Kastettu paikan korjaus
+Vastaanottaja: "Kujansuu, Kari" <kari.kujansuu@gmail.com>
+Kopio: Juha Mäkeläinen <juha.makelainen@iki.fi>
+
+
+Moi,
+yhdessä gedcomissa oli runsaasti syntymäpaikkoja muodossa "(kastettu) Oulu". Ilmeisesti sukututkimusohjelma ei ole tukenut kastetiedon kunnon rekisteröintiä.
+
+Voisitko lisätä paikkojen käsittelyyn säännön, että jos
+
+1 BIRT
+2 PLAC (kastettu) xxx
+
+niin muutetaan muotoon
+
+1 CHR
+2 PLAC xxx
+
+t.
+Pekka
+
+"""
 
 ids = set()
 
