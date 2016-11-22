31f7880a4bfa65f7b2ffa285d6a6b88da53641cc
kari <kari@rf711>
2016-11-22 22:38:23 +0200

    Added documentation in the beginning of the file

--- Gedcom/gedcom_transform.py
+++ Gedcom/gedcom_transform.py
@@ -1,5 +1,68 @@
 #!/usr/bin/env python3
 
+"""
+Generic GEDCOM transformer.
+
+The transforms are specified by separate Python modules ("plugins"). 
+
+The name of the plugin is passed as the first parameter. This can be the name of the Python file ("module.py") 
+or just the name of the module ("module"). In both case the .py file must be in the current directory
+or on the PYTHONPATH.
+
+The second parameter is the name of the input GEDCOM file. This is also the name of the output file.
+
+An optional --encoding parameter can be passed to specify the character encoding used to 
+read and write the GEDCOM files. The default is UTF-8.
+
+The optional parameter --display_changes can be specified to allow the plugins to
+show the modification. The plugin must implement the logic to do that.
+ 
+The optional parameter --dryrun means that the changes are not saved and the input file is not modified.
+
+If the --dryrun parameter is not specified then the original input file is
+renamed by adding a sequence number to the file name and the new version of
+the file is saved with the same name as the input file. These names are displayed at the
+end of the program.
+
+A plugin may contain the following functions:
+
+- add_args(parser)            
+- initialize(args)
+- phase1(args,line,path,tag,value)
+- phase2(args)
+- phase3(args,line,path,tag,value,output_file)
+            
+Function phase1 and phase2 are optional - or they can be defined with an empty body (pass).
+
+Function initialize is called once in the beginning of the transformation.
+
+Function phase1 is called once for each line in the input GEDCOM file. It can be used to collect
+and store information about the GEDCOM to be used in the subsequent phases.
+
+Function phase2 is called once after phase1 but before phase2. 
+
+Function phase3 is called once for each line in the input GEDCOM file. It is called after phase2.
+This function should produce the output GEDCOM by calling output_file.emit
+for each line in the output file. If an input line is not modified then emit should
+be called with the original line as the parameter.
+
+The parameters:
+- "args" is the object returned by ArgumentParser.parse_args.
+- "line" is the original line in the input GEDCOM (unicode string)
+- "path" is the current hierarchy of the GEDCOM tags, e.g @I123@.BIRT.DATE representing the DATE tag
+  inside the BIRT tag for the individual @I123@.
+- "tag" is the current tag (last part of path)
+- "value" is the value for the current tag, e.g. a date or a name
+- "output_file" is a file-like object containing the method emit(string) that is used to produce the output
+- "parser" is the ArgumentParser object of the argparse module
+
+The function add_args is called in the beginning of the program and it allows
+the plugin to add its own arguments for the program. The values of the arguments
+are stored in the "args" object that is passed to the other functions. 
+
+"""
+
+
 import sys
 import os
 import argparse
@@ -80,7 +143,7 @@ def process_gedcom(args,transformer):
 
                        
 def main(): 
-    parser = argparse.ArgumentParser(description='GEDCOM transformation')
+    parser = argparse.ArgumentParser(description='GEDCOM transformations')
     parser.add_argument('transform', help="Name of the transform (Python module)")
     parser.add_argument('input_gedcom', help="Name of the input GEDCOM file")
     #parser.add_argument('output_gedcom', help="Name of the output GEDCOM file; this file will be created/overwritten" ) 
@@ -93,7 +156,7 @@ def main():
     parser.add_argument('--encoding', type=str, default="utf-8",
                         help="UTF-8, ISO8859-1 tai jokin muu")
  
-    if len(sys.argv) > 1 and sys.argv[1][0] == '-' and sys.argv[1] != "-h":
+    if len(sys.argv) > 1 and sys.argv[1][0] == '-' and sys.argv[1] not in ("-h","--help"):
         print("First argument must be the name of the transform")
         return
      
