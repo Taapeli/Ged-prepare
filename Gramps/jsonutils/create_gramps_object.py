05a2b1476387b516f1cfacb310c7e2ce85d0caf5
TimNal <timo.nallikari@gmail.com>
2016-11-24 21:21:17 +0200

    Lisätty result.csv-tiedoston tuottaminen

--- Gramps/jsonutils/create_gramps_object.py
+++ Gramps/jsonutils/create_gramps_object.py
@@ -19,33 +19,40 @@ class CreateGrampsObject:
         self.chgtime = int(time.time())
         print("   chgtime = " + str(self.chgtime))         
 
-    def buildTag(self, tname): 
+    def buildTag(self, tname, thandle = None):
+        if thandle == None:
+            thandle = id.create_id() 
         tag = Tag()
         tag.set_name(tname)
         tag.set_change_time(self.chgtime)
 #        tag.set_color("#EF2929")
-        tag.set_handle(id.create_id())   # 26-merkkinen tunniste
+        tag.set_handle(thandle)   # 26-merkkinen tunniste
 #        print ("Tag ") ; print(tag.to_struct())
-        return (tag)
+        return ([tag, thandle])
     
-    def buildRepository(self, ridno, rname, tag, rtype):
+    def buildRepository(self, ridno, rname, tag, rtype, rhandle):
+        if rhandle == None:
+            rhandle = id.create_id()
         repositoryType = RepositoryType()
         repositoryType.set(rtype)       
     
         repository = Repository()
         repository.set_type(repositoryType)
-        repository.set_handle(id.create_id())
+        repository.set_handle(rhandle)
         repository.set_gramps_id(ridno)
         repository.set_name(rname)
         repository.set_change_time(self.chgtime)
 #       repository.set_color("#000000")
         repository.add_tag(tag.get_handle())
 #        print ("Repository ") ; print(repository.to_struct())
-        return (repository)
+        return ([repository, rhandle])
     
-    def buildSource(self, sidno, stitle, tag, repository):
+    def buildSource(self, sidno, stitle, tag, repository, shandle = None):
+        if shandle == None:
+            shandle = id.create_id()
+#        repositoryType = RepositoryType()
         source = Source()
-        source.set_handle(id.create_id())
+        source.set_handle(shandle)
         source.set_gramps_id(sidno)
         source.set_title(stitle)
         source.add_tag(tag.get_handle())
@@ -55,5 +62,5 @@ class CreateGrampsObject:
         source.set_change_time(self.chgtime)
 #        source.set_color("#000000")
 #        print ("Source ") ; print(source.to_struct())
-        return (source)
+        return ([source, shandle])
 
--- Gramps/jsonutils/write_json_sources.py
+++ Gramps/jsonutils/write_json_sources.py
@@ -6,10 +6,19 @@ Created on 8.11.2016
 '''
 
 import sys
+#from gramps.gen.lib import Repository as repository
+#from gramps.gen.lib import Source as source, Tag
 
 from Gramps.jsonutils.create_gramps_object import CreateGrampsObject
 from sys import argv
+import csv
 
+tag     = None
+thandle = None
+repository = None
+rhandle    = None
+source  = None
+shandle = None
 
 def main(argv):
 #    def main(argv = ["write_json_sources", "C:/Temp/", "Lahteet.txt", "JsonIn.json", 500000, 500000]):
@@ -23,6 +32,7 @@ def main(argv):
     fdir = argv[1]                   # Directory for input and output files
     fin  = fdir + argv[2]            # Input file
     fout = fdir + argv[3]            # Output file
+    cout = fdir + "result.csv"
     repository_idno = int(argv[4])   # Start value for repository id numbers
     repository_incr = int(argv[5])   # Increment value  for repository id numbers
     source_idno = 0                  # Start value for source id number within repository
@@ -34,48 +44,59 @@ def main(argv):
     tag  = None
     rtag = None            # Tag used for repositories
     stag = None            # Tag used for sources
-    
-    repository = None
-    source = None
-    
+        
     cgo = CreateGrampsObject()
       
     try:
-        with open(fout, "w", encoding="UTF-8") as j_out:
-            rectype = ""
-            arctype = ""
-            with open(fin, "r") as t_in:
-                for line in t_in:
-                    ttext =  line.strip('\n ')
-                    print(ttext)
-                    rectype = ttext[0]         # Object type = Gramps object id prefix character
-                    recobj  = ttext[2]         # Tag: host object type   Repository: repository type
-                    idno = ttext[5:11]         # Alternative way to assign Gramps object id's
-                    otext = ttext[15:len(ttext)]
-#                    print("-" + rectype + "-" + idno + "-" + otext + "-")
-                    
-                    if rectype == "T":
-                        tag = cgo.buildTag(otext)
-                        if   recobj == "R": rtag = tag      # The tag for repositories
-                        elif recobj == "S": stag = tag      # The tag for sources
-                        print(tag.to_struct(), file=j_out)
-                                   
-                    elif rectype == "R":
-                        arctype = int(recobj)
-                        repository_idno = repository_idno + repository_incr
-                        ridno = rectype + "ref" + str(arctype * 1000 + repository_idno)
-                        repository = cgo.buildRepository(ridno, otext, rtag, int(recobj))
-                        print(repository.to_struct(), file=j_out)
-   
-                    elif rectype == "S":
-                        source_idno = source_idno + 1
-                        sidno = rectype +"ref" + str(arctype) + str(repository_idno * 1000 + source_idno)
-#                        sidno = ""
-                        source = cgo.buildSource(sidno, otext, stag, repository)
-                        print(source.to_struct(), file=j_out)
-                    else:
-                        print("Unknown rectype: + rectype")
-            print("Input file handled.")
+
+        with open(cout, "w", newline = "\n") as csv_out:
+            r_writer = csv.writer(csv_out, delimiter=',')
+            with open(fout, "w", encoding="UTF-8") as j_out:
+                rectype = ""
+                arctype = ""
+                with open(fin, "r") as t_in:
+
+                    for line in t_in:
+                        rhandle = None
+                        shandle = None
+                        
+                        ttext =  line.strip('\n ')
+                        print(ttext)
+                        rectype = ttext[0]         # Object type = Gramps object id prefix character
+                        recobj  = ttext[2]         # Tag: host object type   Repository: repository type
+    #                    idno = ttext[5:11]         # Alternative way to assign Gramps object id's
+                        otext = ttext[15:len(ttext)]
+    #                    print("-" + rectype + "-" + idno + "-" + otext + "-")
+                        
+                        if rectype == "T":
+                            tagr = cgo.buildTag(otext, thandle)
+                            if   recobj == "R": rtag = tagr[0]     # The tag for repositories
+                            elif recobj == "S": stag = tagr[0]     # The tag for sources
+                            print(tagr[0].to_struct(), file=j_out)
+                                       
+                        elif rectype == "R":
+                            arctype = int(recobj)
+                            repository_idno = repository_idno + repository_incr
+                            ridno = rectype + "ref" + str(arctype) + str(repository_idno)
+                            repor = cgo.buildRepository(ridno, otext, rtag, int(recobj), rhandle)
+                            print(repor[0].to_struct(), file=j_out)
+                            try: 
+                                r_writer.writerow([ridno, repor[1], otext])
+                            except:    
+                                print("Error writing R-csv")
+                        elif rectype == "S":
+                            source_idno = source_idno + 1
+                            sidno = rectype +"ref" + str(arctype) + str(repository_idno * 1000 + source_idno)
+                            sour = cgo.buildSource(sidno, otext, stag, repor[0], shandle)
+                            print(sour[0].to_struct(), file=j_out)
+    #                        shandle = source.get_handle()
+                            try:
+                                r_writer.writerow([sidno, sour[1], otext])
+                            except:    
+                                print("Error writing S-csv")
+                        else:
+                            print("Unknown rectype: + rectype")
+                print("Input file handled.")
     except  IOError: 
         print(IOError.winerror)
         print("IOError in j_out handling")
