99515245188ab65e2d2e51c6536f56073a183ccb
TimNal <timo.nallikari@gmail.com>
2016-11-27 08:25:51 +0200

    Lisätty output-csv:n tiedostonimi

--- Gramps/jsonutils/test/test_write_json_sources2.py
+++ Gramps/jsonutils/test/test_write_json_sources2.py
@@ -6,6 +6,5 @@ Created on 16.11.2016
 
 from Gramps.jsonutils import write_json_sources
 
-
 if __name__ == '__main__':
     write_json_sources.main(["write_json_sources", "C:/Temp/", "Sources.csv", "Result.json", "Result.csv", 1000, 10])
