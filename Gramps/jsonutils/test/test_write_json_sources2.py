ecbba951c52e741e5eab5c4f3e366cbc4b695156
TimNal <timo.nallikari@gmail.com>
2016-11-27 07:07:46 +0200

    Uudet testiaineistot ja tulokset

---
+++ Gramps/jsonutils/test/test_write_json_sources2.py
@@ -0,0 +1,11 @@
+'''
+Created on 16.11.2016
+
+@author: Timo Nallikari
+'''
+
+from Gramps.jsonutils import write_json_sources
+
+
+if __name__ == '__main__':
+    write_json_sources.main(["write_json_sources", "C:/Temp/", "Sources.csv", "Result.json", "Result.csv", 1000, 10])
