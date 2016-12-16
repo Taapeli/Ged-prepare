db144d1ac4cbd2602ae5cd40b4e64c3e6fb52645
jorma-h <jorma.haapasalo@pp.inet.fi>
2016-12-16 22:50:29 +0200

    Some more error handling added

--- Bus/read_xml_write_neo4j.py
+++ Bus/read_xml_write_neo4j.py
@@ -13,8 +13,7 @@ import sys
 import time
 import xml.dom.minidom
 import argparse
-
-
+from sys import stderr
 from classes.genealogy import *
 
 connect_db()
@@ -41,24 +40,34 @@ def handle_citations(collection):
             citation_dateval = citation.getElementsByTagName('dateval')[0]
             if citation_dateval.hasAttribute("val"):
                 c.dateval = citation_dateval.getAttribute("val")
+        elif len(citation.getElementsByTagName('dateval') ) > 1:
+            print("Error: More than one dateval tag in a citation")
     
         if len(citation.getElementsByTagName('page') ) == 1:
             citation_page = citation.getElementsByTagName('page')[0]
             c.page = citation_page.childNodes[0].data
+        elif len(citation.getElementsByTagName('page') ) > 1:
+            print("Error: More than one page tag in a citation")
     
         if len(citation.getElementsByTagName('confidence') ) == 1:
             citation_confidence = citation.getElementsByTagName('confidence')[0]
             c.confidence = citation_confidence.childNodes[0].data
+        elif len(citation.getElementsByTagName('confidence') ) > 1:
+            print("Error: More than one confidence tag in a citation")
     
         if len(citation.getElementsByTagName('noteref') ) == 1:
             citation_noteref = citation.getElementsByTagName('noteref')[0]
             if citation_noteref.hasAttribute("hlink"):
                 c.noteref_hlink = citation_noteref.getAttribute("hlink")
+        elif len(citation.getElementsByTagName('noteref') ) > 1:
+            print("Error: More than one noteref tag in a citation")
     
         if len(citation.getElementsByTagName('sourceref') ) == 1:
             citation_sourceref = citation.getElementsByTagName('sourceref')[0]
             if citation_sourceref.hasAttribute("hlink"):
                 c.sourceref_hlink = citation_sourceref.getAttribute("hlink")
+        elif len(citation.getElementsByTagName('sourceref') ) > 1:
+            print("Error: More than one sourceref tag in a citation")
                 
         c.save()
         
@@ -92,21 +101,29 @@ def handle_events(collection):
         if len(event.getElementsByTagName('type') ) == 1:
             event_type = event.getElementsByTagName('type')[0]
             e.type = event_type.childNodes[0].data
+        elif len(event.getElementsByTagName('type') ) > 1:
+            print("Error: More than one type tag in an event")
     
         if len(event.getElementsByTagName('dateval') ) == 1:
             event_dateval = event.getElementsByTagName('dateval')[0]
             if event_dateval.hasAttribute("val"):
                 e.date = event_dateval.getAttribute("val")
+        elif len(event.getElementsByTagName('dateval') ) > 1:
+            print("Error: More than one dateval tag in an event")
     
         if len(event.getElementsByTagName('place') ) == 1:
             event_place = event.getElementsByTagName('place')[0]
             if event_place.hasAttribute("hlink"):
                 e.place_hlink = event_place.getAttribute("hlink")
+        elif len(event.getElementsByTagName('place') ) > 1:
+            print("Error: More than one place tag in an event")
     
         if len(event.getElementsByTagName('citationref') ) == 1:
             event_citationref = event.getElementsByTagName('citationref')[0]
             if event_citationref.hasAttribute("hlink"):
                 e.citationref_hlink = event_citationref.getAttribute("hlink")
+        elif len(event.getElementsByTagName('citationref') ) > 1:
+            print("Error: More than one citationref tag in an event")
                 
         e.save()
         
@@ -140,16 +157,22 @@ def handle_families(collection):
             family_rel = family.getElementsByTagName('rel')[0]
             if family_rel.hasAttribute("type"):
                 f.rel_type = family_rel.getAttribute("type")
+        elif len(family.getElementsByTagName('rel') ) > 1:
+            print("Error: More than one rel tag in a family")
     
         if len(family.getElementsByTagName('father') ) == 1:
             family_father = family.getElementsByTagName('father')[0]
             if family_father.hasAttribute("hlink"):
                 f.father = family_father.getAttribute("hlink")
+        elif len(family.getElementsByTagName('father') ) > 1:
+            print("Error: More than one father tag in a family")
     
         if len(family.getElementsByTagName('mother') ) == 1:
             family_mother = family.getElementsByTagName('mother')[0]
             if family_mother.hasAttribute("hlink"):
                 f.mother = family_mother.getAttribute("hlink")
+        elif len(family.getElementsByTagName('mother') ) > 1:
+            print("Error: More than one mother tag in a family")
     
         if len(family.getElementsByTagName('eventref') ) >= 1:
             for i in range(len(family.getElementsByTagName('eventref') )):
@@ -230,6 +253,8 @@ def handle_people(collection):
         if len(person.getElementsByTagName('gender') ) == 1:
             person_gender = person.getElementsByTagName('gender')[0]
             p.gender = person_gender.childNodes[0].data
+        elif len(person.getElementsByTagName('gender') ) > 1:
+            print("Error: More than one gender tag in a person")
     
         if len(person.getElementsByTagName('name') ) >= 1:
             for i in range(len(person.getElementsByTagName('name') )):
@@ -242,17 +267,28 @@ def handle_people(collection):
     
                 if len(person_name.getElementsByTagName('first') ) == 1:
                     person_first = person_name.getElementsByTagName('first')[0]
-                    if len(person_first.childNodes ) == 1:
+                    if len(person_first.childNodes) == 1:
                         pname.first = person_first.childNodes[0].data
+                    elif len(person_first.childNodes) > 1:
+                        print("Error: More than one child node in a first name of a person")
+                elif len(person_name.getElementsByTagName('first') ) > 1:
+                    print("Error: More than one first name in a person")
     
                 if len(person_name.getElementsByTagName('surname') ) == 1:
                     person_surname = person_name.getElementsByTagName('surname')[0]
                     if len(person_surname.childNodes ) == 1:
                         pname.surname = person_surname.childNodes[0].data
+                    elif len(person_surname.childNodes) > 1:
+                        print("Error: More than one child node in a surname of a person")
+                elif len(person_name.getElementsByTagName('surname') ) > 1:
+                    print("Error: More than one surname in a person")
     
                 if len(person_name.getElementsByTagName('suffix') ) == 1:
                     person_suffix = person_name.getElementsByTagName('suffix')[0]
                     pname.suffix = person_suffix.childNodes[0].data
+                elif len(person_name.getElementsByTagName('suffix') ) > 1:
+                    print("Error: More than one suffix in a person")
+                    
                 p.name.append(pname)
     
         if len(person.getElementsByTagName('eventref') ) >= 1:
@@ -308,6 +344,8 @@ def handle_places(collection):
         if len(placeobj.getElementsByTagName('ptitle') ) == 1:
             placeobj_ptitle = placeobj.getElementsByTagName('ptitle')[0]
             place.ptitle = placeobj_ptitle.childNodes[0].data
+        elif len(placeobj.getElementsByTagName('ptitle') ) > 1:
+            print("Error: More than one ptitle in a place")
     
         if len(placeobj.getElementsByTagName('pname') ) >= 1:
             for i in range(len(placeobj.getElementsByTagName('pname') )):
@@ -319,6 +357,8 @@ def handle_places(collection):
             placeobj_placeref = placeobj.getElementsByTagName('placeref')[0]
             if placeobj_placeref.hasAttribute("hlink"):
                 place.placeref_hlink = placeobj_placeref.getAttribute("hlink")
+        elif len(placeobj.getElementsByTagName('placeref') ) > 1:
+            print("Error: More than one placeref in a place")
                 
         place.save()
         
@@ -351,10 +391,14 @@ def handle_repositories(collection):
         if len(repository.getElementsByTagName('rname') ) == 1:
             repository_rname = repository.getElementsByTagName('rname')[0]
             r.rname = repository_rname.childNodes[0].data
+        elif len(repository.getElementsByTagName('rname') ) > 1:
+            print("Error: More than one rname in a repository")
     
         if len(repository.getElementsByTagName('type') ) == 1:
             repository_type = repository.getElementsByTagName('type')[0]
             r.type =  repository_type.childNodes[0].data
+        elif len(repository.getElementsByTagName('type') ) > 1:
+            print("Error: More than one type in a repository")
     
         r.save()
         
@@ -387,16 +431,22 @@ def handle_sources(collection):
         if len(source.getElementsByTagName('stitle') ) == 1:
             source_stitle = source.getElementsByTagName('stitle')[0]
             s.stitle = source_stitle.childNodes[0].data
+        elif len(source.getElementsByTagName('stitle') ) > 1:
+            print("Error: More than one stitle in a source")
     
         if len(source.getElementsByTagName('noteref') ) == 1:
             source_noteref = source.getElementsByTagName('noteref')[0]
             if source_noteref.hasAttribute("hlink"):
                 s.noteref_hlink = source_noteref.getAttribute("hlink")
+        elif len(source.getElementsByTagName('noteref') ) > 1:
+            print("Error: More than one noteref in a source")
     
         if len(source.getElementsByTagName('reporef') ) == 1:
             source_reporef = source.getElementsByTagName('reporef')[0]
             if source_reporef.hasAttribute("hlink"):
-                s.reporef_hlink = source_reporef.getAttribute("hlink")
+                s.reporef_hlink = source_reporef.getAttribute("reporef")
+        elif len(source.getElementsByTagName('noteref') ) > 1:
+            print("Error: More than one reporef in a source")
     
         s.save()
         
@@ -412,17 +462,22 @@ def handle_sources(collection):
 def process_xml(args):
 
     # Open XML document using minidom parser
-    DOMTree = xml.dom.minidom.parse(open(args.input_xml))
-    collection = DOMTree.documentElement
-
-    handle_notes(collection)
-    handle_repositories(collection)
-    handle_places(collection)
-    handle_sources(collection)
-    handle_citations(collection)
-    handle_events(collection)
-    handle_people(collection)
-    handle_families(collection)
+    try:
+        DOMTree = xml.dom.minidom.parse(open(args.input_xml))
+        collection = DOMTree.documentElement
+    
+        handle_notes(collection)
+        handle_repositories(collection)
+        handle_places(collection)
+        handle_sources(collection)
+        handle_citations(collection)
+        handle_events(collection)
+        handle_people(collection)
+        handle_families(collection)
+    except FileNotFoundError:
+        print("Tiedostoa '{}' ei ole!".format(args.input_xml), file=stderr)
+    except Exception as err:
+        print("Virhe: {0}".format(err), file=stderr)
 
 
 
