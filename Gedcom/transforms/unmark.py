9b7d0f3489f2c6df498df8f5ea6c52941f03292a
Kari Kujansuu <kari.kujansuu@gmail.com>
2016-12-24 22:40:45 +0200

    Add transofmr 'unmark'

---
+++ Gedcom/transforms/unmark.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Restores marked tags: <tag>-X -> <tag>
+"""
+
+version = "1.0"
+
+ids = set()
+
+def add_args(parser):
+    pass
+
+def initialize(args):
+    pass
+
+def phase3(args,line,path,tag,value,f):
+    if tag.endswith("-X"):
+        tag = tag[:-2]
+        level = line.split()[0]
+        line = "{} {} {}".format(level,tag,value)
+    f.emit(line)
+
+
